10f104c234b39ce3878559a0817fd55a26b8a99b
zhkgo <zhkgogogo@foxmail.com>
2021-10-22 15:13:31 +0800

    接入自研脑电帽，前端的数据显示规则由标准化（standardization）,变为归一化(minmax)。

---
+++ api/cytonParser.py
@@ -0,0 +1,95 @@
+import argparse
+from io import SEEK_CUR
+import time
+import numpy as np
+
+import brainflow
+import threading
+from socket import *
+from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+
+class CytonDevice:
+    def __init__(self, serial_port="COM6 ", time_length=0.04, port="",host="",name="", sr=250, borad_id=0):
+        # board_id: 选取pcb类型，0是8通cyton, 1是16通cyton
+        self.params = BrainFlowInputParams()
+        self.params.ip_port = 0
+        self.params.serial_port = serial_port
+        self.params.mac_address = ""
+        self.params.other_info = ""
+        self.params.serial_number = ""
+        self.params.ip_address = "127.0.0.1"
+        self.params.ip_protocol = 0  # 0: None
+        self.params.timeout = 100
+        self.params.file = "params.txt"
+
+        self.end = 0
+        self.name = name
+        self.slice = int(time_length * sr)
+        self.time_length = time_length
+        self.chns = ["Fp1", "Fp2", "C3", "C4", "P7", "P8", "O1", "O2"]
+
+        self.board = BoardShim(board_id=borad_id, input_params=self.params)
+
+    def reinit(self):
+        self.board.stop_stream()
+        self.board.release_session()
+        self.crate_batch()
+
+    def create_batch(self,ch_names=["Fp1", "Fp2", "C3", "C4", "P7", "P8", "O1", "O2"], __=1):
+        self.signals = np.zeros((len(ch_names), 3600000))
+        self.board.prepare_session()
+        self.board.start_stream(1800 * 250, '')
+
+    def parse_data(self):
+        # 舍弃一段数据
+        _ = self.board.get_board_data()
+        while True:
+            try:
+                # 得到指定长度的数据片
+                time.sleep(self.time_length)
+                data = self.board.get_board_data()
+                # print(data[1:9,:])
+                # span = data[22][-1]-data[22][0]
+                self.signals[:, self.end:self.end + data.shape[1]] = data[1:9,:]
+                self.end += data.shape[1]
+            except Exception as e:
+                print(e)
+                break
+
+    def get_batch(self, startPos, maxlength=200):
+        if startPos <= -1:
+            startPos = self.end - maxlength
+        rend = min(self.end, startPos + maxlength)
+        arr = self.signals[:, startPos:rend]
+        return arr, rend
+
+    def saveData(self, name, startpos):
+        ctime = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        savepathvalue = "data/" + name + ctime + ".npy"
+        np.save(savepathvalue, self.signals[:, startpos:self.end])
+
+    def get_device_info(self):
+        board_id = self.board.get_board_id()
+        info = self.board.get_board_descr(board_id=board_id)
+        return info["eeg_names"], info["sampling_rate"]
+
+    def close(self, savefile=True):
+        time.sleep(0.2)
+        self.board.stop_stream()
+        self.board.release_session()
+
+
+if __name__ == "__main__":
+    cyton = CytonDevice()
+    cyton.create_batch()
+    cyton.parse_data()
+
+    # chns, sr, names = cyton.get_device_info()
+    # print(chns, " ", sr, " ", names)
+    # for i in range(10):
+    #     data, span = cyton.get_batch()
+    #     print(i)
+    #     print(data.shape)
+    #     print(span)
---
+++ controller_ziyan.py
@@ -0,0 +1,192 @@
+"""
+Created on Mon Jun 28 15:08:02 2021
+
+@author: zhkgo
+"""
+
+from experiment import Experiment
+from api.cytonParser import CytonDevice as BCI
+# from api.dsiParse import DSIDevice as BCI
+from api.reid import ReIDTCP
+import configparser
+from bcifilter import BciFilter
+from datatable import VideoDeepLink,Linker
+from flask import Flask,request
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+from threading import Lock
+from myresponse import success,fail
+import importlib
+import traceback
+import time
+import numpy as np
+thread_lock = Lock()
+_thread=None
+
+conf = configparser.ConfigParser()
+experiment=None
+reID=None
+linker=Linker()
+app = Flask(__name__,static_url_path="")
+app.config['JSON_AS_ASCII'] = False
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, supports_credentials=True)
+socketio = SocketIO(app)
+
+
+@app.route("/api/bcigo") 
+def bcigo():
+    try:
+        bciReady()
+    except Exception as e:
+        return fail(str(e))
+    # return success({"channels":experiment.channels})
+    print(experiment.channels)
+    return success({"channels":["Fp1", "Fp2", "C3", "C4", "P7", "P8", "O1", "O2"]})
+
+@app.route('/api/getdata')
+def getdata():
+    global experiment
+    # print("TCP END WHEN GET DATA",experiment.tcp.end)
+    try:
+        timeend=int(request.args.get('timeend'))
+        arr,rend=experiment.getData(timeend,zerobegin=True)
+        # arr=(arr-experiment.means)/experiment.sigmas
+        arr=(arr-experiment.minnum)/(experiment.maxnum-experiment.minnum)
+        # print(arr.tolist())
+    except Exception as e:
+        print("ss")
+        traceback.print_exc()
+        return fail(str(e))
+    #print("返回数据维度：", np.array(arr).shape)
+    # print(np.array(arr).shape)
+    # ['Fz','Cz','Pz','P3','P4','P7','P8','Oz','O1','O2','T7','T8']
+    #选择部分通道显示
+    # idxs=[0,6,3,2,4,10,11]
+    # arr=arr[idxs]
+    # print(np.array(arr).shape)
+    return success({"data":arr.tolist(),'timeend':rend})
+#开始记录数据
+@app.route("/api/startRecord")
+def startRecord():
+    global experiment
+    if experiment==None:
+        return fail("请先创建实验")
+    try:
+        experiment.startRecord()
+    except Exception as e:
+        traceback.print_exc()
+        return fail(str(e))
+    return success("开始记录当前脑纹数据")
+
+#停止记录数据
+@app.route("/api/stopRecord")
+def stopRecord():
+    global experiment
+    if experiment==None:
+        return fail("请先创建实验")
+    try:
+        name=request.args.get('subjectName')
+        experiment.stopRecord(subjectName=name)
+    except Exception as e:
+        traceback.print_exc()
+        return fail(str(e))
+    return success("当前脑纹采集成功")
+
+@app.route("/api/startJudge")
+def startJudge():
+    global experiment
+    subjects=["刘国文","章杭奎","潘泽宇","金宣妤"]
+    res=None
+    if experiment==None:
+        return fail("请先创建实验")
+    try:
+        rr=[]
+        for i in range(100):
+            rr.append(experiment.predictOnce())
+            print(subjects[rr[-1]],end=' ')
+            time.sleep(0.05)
+        print("")
+        res=subjects[max(rr,key=rr.count)] #小数据范围有效
+    except Exception as e:
+        traceback.print_exc()
+        return success(subjects[0])
+        return fail(str(e))
+    return success(res)
+
+@app.route("/api/reviseBaseline")
+def reviseBaseline():
+    global experiment
+    try:
+        experiment.reviseBaseline()
+    except Exception as e:
+        print(e)
+        return fail(str(e))
+    return success("修正基线成功")
+
+@app.route("/api/closeBCI")
+def closeBCI():
+    global experiment
+    experiment.finish(savefile=False)
+    return success()
+
+
+'''准备脑电接口'''
+def bciReady(filename='config1.ini'):
+    global experiment
+    if experiment!=None:
+        experiment.finish()
+    conf.read(filename)
+    experiment=Experiment()
+    cur=conf['experiment']
+    sessions=int(cur['session'])
+    fitSessions=int(cur['fitsessions'])
+    trials=int(cur['trials'])
+    duration=int(cur['duration'])
+    interval=int(cur['interval'])
+    tmin=int(cur['tmin'])
+    tmax=int(cur['tmax'])
+    device=int(cur['device'])
+    skipinterval=int(cur['skipinterval'])
+    experiment.setParameters(sessions,fitSessions,trials,duration,interval,tmin,tmax,device,skipinterval)
+    print("实验创建成功")
+    
+    cur=conf['filter']
+    sampleRateFrom=int(cur['sampleRateFrom'])
+    sampleRateTo=int(cur['sampleRateTo'])
+    low=float(cur['low'])
+    high=float(cur['high'])
+    channels=cur['channels'].split(',')
+    idxs=experiment.set_channel(channels)
+
+    mfilter=BciFilter(low,high,sampleRateFrom,sampleRateTo,idxs)
+    experiment.set_filter(mfilter)
+    print("滤波-通道选择器初始化成功")
+    
+    module=importlib.import_module(conf['model']['path'])
+    for name in module.getClassName():
+        content="globals()['"+name+"']=module."+name
+        exec(content)
+    experiment.set_classfier(module.getModel())
+    print("脑电判别模型准备成功")
+    
+    cur=conf['bcitcp']
+    host=cur['host']
+    port=int(cur['port'])
+    tcpname=cur['name']
+    tcp=BCI(host=host,port=port,name=tcpname)
+    print("tcp: ",tcp)
+    ch_nums=experiment.device_channels
+    tcp.create_batch(ch_nums)
+    experiment.set_dataIn(tcp)
+    experiment.start_tcp()  #start To SaveData
+    time.sleep(3)   #先接一部分数据
+    print("脑电数据接入成功")
+'''准备REID接口'''
+# def reIDReady(filename='config.ini'):
+#     global reID
+#     conf.read(filename)
+#     cur=conf['reidtcp']
+#     reID=ReIDTCP(host=cur['host'],port=int(cur['port']))
+if __name__=='__main__':    
+    socketio.run(app, host='0.0.0.0',port=80, debug=False)
